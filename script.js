0a9b146ba4e7406ccd9b37003ea9e533016cf6fa
Jessie <zhangqian006@gmail.com>
2024-07-03 13:50:27 +1000

    add login form

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
 "use strict";
 
-const section = document.querySelector(".projects");
-const initalCoords = section.getBoundingClientRect();
-console.log(initalCoords);
-window.addEventListener("scroll", function () {
-  console.log(window.scrollY);
+// Sticky navigation
+const header = document.querySelector(".header");
+const navigation = document.querySelector(".navigation");
+const navHeight = navigation.getBoundingClientRect().height;
 
-  if (window.scrollY > initalCoords.top) nav.classList.add("sticky");
-  else nav.classList.remove("sticky");
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) navigation.classList.add("sticky");
+  else navigation.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+// Handle form
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const comment = document.getElementById("comment").value;
+
+  if (!name || email || subject || comment) {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  const formData = {
+    name: name,
+    email: email,
+    subject: subject,
+    comment: comment,
+  };
+  fetch("'file.php'", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert("Message sent successfully!");
+      document.getElementById("contactForm").reset(); // Resets the form
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("There was an error sending your message. Please try again.");
+    });
 });
